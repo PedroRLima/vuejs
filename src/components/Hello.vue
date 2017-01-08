baf7b85fcf7ed22c6c8a852f5a20bf489331cf5f
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-01-08 11:09:41 -0200

    just playing with the basics

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div>
+      <ul>
+        <li v-for="user in users">
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+    </div>
+    <hr/>
+    <div>
+      <input type="text" v-model="input_val">
+    </div>
+    <div>
+      Input Value: <span v-text="input_val"></span>
+    </div>
+    <div>
+      <button class="btn btn-primary" v-on:click="counter++">You've clicked this button {{counter}}
+    </div>
   </div>
 </template>
 
@@ -25,7 +26,14 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      users: [
+        {firstname: 'Sebastian', lastname: 'Eschweiler'},
+        {firstname: 'Pedro', lastname: 'Silva'},
+        {firstname: 'Joana', lastname: 'Silva'}
+      ],
+      input_val: '',
+      counter: 0
     }
   }
 }
